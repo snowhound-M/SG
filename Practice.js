4becd893953c798904951a40247ed7f4908a9dc4
snowhound-M <atulpundir147@gmail.com>
2024-08-07 00:17:18 +0530

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -4,9 +4,32 @@ function myFunction(x, y) {
 
 myFunction(3, 4);
 
+function myFunction(x, y) {
+    return x + y;
+}
+
+let newReturnValue = myFunction(3, 4);
+console.log(newReturnValue);
 
 let returnValue = function myFunction(x, y) {
     console.log(x + y);
 };
 
 returnValue(3, 4);
+
+
+let returnValue = (x, y) => {
+    console.log(x + y);
+};
+
+/*
+
+let returnValue = (x, y) => console.log(x + y);
+};
+
+*/
+
+returnValue(3, 4);
+
+
+
