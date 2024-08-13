a890fbb85d828d49a0d5e971d0c7450b42bdc27d
snowhound-M <atulpundir147@gmail.com>
2024-08-13 13:09:42 +0530

    Update react-app.jsx

--- react-app.jsx
+++ react-app.jsx
@@ -1,1 +1,44 @@
+import { React } from 'react';
 
+function App() {
+  const [myTodoList, setMyTodoList] = useState([]);
+  const [myNewTask, setMyNewTask] = useState("");
+
+  function addTask() {
+    setMyNewTask(event.target.value);
+  }
+
+  function addTaskToList() {
+    const newTask = {
+      id: myTodoList.length === 0 ? 1 : myTodoList[myTodoList.length - 1].id + 1,
+      taskName: myNewTask
+    };
+    
+    setMyTodoList([...myTodoList, newTask]);
+  }
+
+  function deleteTaskFromList() {
+    setMyNewTask(event.target.value);
+  }
+  
+  return (
+    <>
+      <div>
+        <h1>TO-DO LIST</h1>
+      </div>
+      <div>
+        <h3>ADD YOUR TASKS HERE</h3>
+        <input type="text" onChange={addTask}></input>
+        <button onClick={addTaskToList}>ADD</button>
+      </div>
+      <div>
+        <h3>YOUR CURRENT TASKS</h3>
+        {myTodoList.map((task) => {return ({task.id}).  ({task.taskName})  <button> X </button>})};
+      </div>
+      <div>
+        <h3> YOUR DELETED TASKS</h3>
+        {}
+      </div>
+    </>
+  );
+}
